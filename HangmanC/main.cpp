5ee9571421ecbad2b598b04650b712b1798f3cea
pixe <pijuspovilas@yahoo.com>
2022-08-22 21:51:20 +0300

    Added several methods to display text, implemented intro prolog

--- HangmanC/main.cpp
+++ HangmanC/main.cpp
@@ -5,18 +5,77 @@
 #include <ctime>
 #include <cctype>
 
-char confirAction(std::string pQuestion);
+bool confirAction(const std::string &pQuestion);
+void displayProlog();
+void initGameVariables();
+void displayMaskedWord();
+void playerGuess();
+
+const std::vector<std::string> gWordDict = { "hangman", "orange", "battleship", "congratulation", "imagination", "advertisement"};
+std::vector<char> gLettersGuessed;
+std::string gWordToGuess;
+std::string gMaskedWord;
+bool vGameLoop = false;
 
 int main()
 {
-	char pTest = confirAction("Are you ready to play?");
+	displayProlog();
+	vGameLoop = confirAction("Are you ready to play?");
 
-	std::cout << pTest;
+	while (vGameLoop)
+	{
+		initGameVariables();
+		displayMaskedWord();
+		playerGuess();
+	}
 
 	return 0;
 }
 
-char confirAction(std::string pQuestion)
+void playerGuess()
+{
+	char guess;
+	std::cout << "Guess letter:" << std::endl;
+	//do
+	//{
+	//	std::cin >> guess;
+	//	if (std::count(gLettersGuessed.begin(), gLettersGuessed.end(), guess))
+	//	{
+	//		std::cout << "You already guessed this letter. Try again." << std::endl;
+	//	}
+	//	else
+	//	{
+	//		gLettersGuessed.push_back(guess);
+	//	}
+	//	
+	//}
+}
+
+void displayMaskedWord()
+{
+	std::cout << gMaskedWord << std::endl;
+}
+
+void initGameVariables()
+{
+	std::srand(std::time(0));
+	int randWordIndex = std::rand() % gWordDict.size() + 1;
+
+	gWordToGuess = gWordDict[randWordIndex];
+	gMaskedWord.assign(gWordToGuess.size(), '-');
+}
+
+void displayProlog()
+{
+	std::cout << "Welcom to Hangman! Prepare to spend some time on guessing secret words!" << std::endl;
+	std::cout << "Several rules before playing:" << std::endl;
+	std::cout << "* You have only 7 guesses" << std::endl;
+	std::cout << "* Only one guess per letter is allowed" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Brace your self and good luck in the game!! Don't leave hanging there!\n" << std::endl;
+}
+
+bool confirAction(const std::string &pQuestion)
 {
 	char pUserAction;
 
@@ -29,5 +88,5 @@ char confirAction(std::string pQuestion)
 
 	} while (pUserAction != 'y' && pUserAction != 'n');
 
-	return pUserAction;
+	return pUserAction == 'y' ? true : false;
 }
